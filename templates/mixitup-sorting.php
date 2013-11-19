788f932167283e948cf8b0d92146e7704d104be8
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-19 12:39:58 +0200

    added mixitup-sorting

---
+++ templates/mixitup-sorting.php
@@ -0,0 +1,7 @@
+<div class="btn-group pull-left">
+	<li class="sort btn btn-default active" data-sort="default"><?php _e( 'Default', 'shoestrap_edd' ); ?></li>
+	<li class="sort btn btn-default" data-sort="data-name" data-order="desc"><?php _e( 'Name', 'shoestrap_edd' ); ?> <i class="el-icon-arrow-down"></i></li>
+	<li class="sort btn btn-default" data-sort="data-name" data-order="asc"><?php _e( 'Name', 'shoestrap_edd' ); ?> <i class="el-icon-arrow-up"></i></li>
+	<li class="sort btn btn-default" data-sort="data-price" data-order="desc"><?php _e( 'Price', 'shoestrap_edd' ); ?> <i class="el-icon-arrow-down"></i></li>
+	<li class="sort btn btn-default" data-sort="data-price" data-order="asc"><?php _e( 'Price', 'shoestrap_edd' ); ?> <i class="el-icon-arrow-up"></i></li>
+</div> 
