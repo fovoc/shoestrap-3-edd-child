9f4b003fcbf29fe329943e0d104633d071bc9c57
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-19 12:09:13 +0200

    dleting archive file

--- archive-download.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ( !is_tax( 'download_category' ) ) :
-	get_template_part( 'templates/mixitup', 'download_category' );
-elseif ( !is_tax( 'download_tag' ) ) :
-	get_template_part( 'templates/mixitup', 'download_tag' );
-endif;
