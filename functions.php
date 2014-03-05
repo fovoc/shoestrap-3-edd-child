b2bdd1f72e96bce8ca83be4e365c717b67fe0cf8
Aristeides Stathopoulos <aristath@gmail.com>
2014-03-05 17:47:25 +0200

    change the way files are loaded

--- functions.php
+++ functions.php
@@ -1,29 +1,18 @@
 <?php
 
-if ( !defined( 'REDUX_OPT_NAME' ) )
-	define( 'REDUX_OPT_NAME', 'shoestrap' );
-
 // Prioritize loading of some necessary core modules
-if ( file_exists( get_template_directory() . '/lib/modules/redux/module.php' ) ) :
-	require_once get_template_directory() . '/lib/modules/redux/module.php';
-endif;
-if ( file_exists( get_template_directory() . '/lib/modules/core/module.php' ) ) :
-	require_once get_template_directory() . '/lib/modules/core/module.php';
-endif;
-if ( file_exists( get_template_directory() . '/lib/modules/layout/module.php' ) ) :
-	require_once get_template_directory() . '/lib/modules/layout/module.php';
-endif;
-if ( file_exists( get_template_directory() . '/lib/modules/blog/module.php' ) ) :
-	require_once get_template_directory() . '/lib/modules/blog/module.php';
-endif;
 
-// Load the EDD admin options
-require_once get_stylesheet_directory() . '/lib/admin.php';
 
 // Load the EDD-Specific functions
-if ( class_exists( 'Easy_Digital_Downloads' ) ) :
-	require_once get_stylesheet_directory() . '/lib/edd-functions.php';
-	require_once get_stylesheet_directory() . '/lib/edd-widgets.php';
-	require_once get_stylesheet_directory() . '/lib/addons/edd-simple-shipping.php';
-	require_once get_stylesheet_directory() . '/lib/addons/edd-variable-pricing-switcher.php';
-endif;
+
+function shoestrap_edd_include_files() {
+	if ( class_exists( 'Easy_Digital_Downloads' ) ) {
+		require_once dirname( __FILE__ ) . '/lib/admin.php';
+		require_once dirname( __FILE__ ) . '/lib/edd-functions.php';
+		require_once dirname( __FILE__ ) . '/lib/edd-shortcodes.php';
+		require_once dirname( __FILE__ ) . '/lib/edd-widgets.php';
+		require_once dirname( __FILE__ ) . '/lib/addons/edd-simple-shipping.php';
+		require_once dirname( __FILE__ ) . '/lib/addons/edd-variable-pricing-switcher.php';
+	}
+}
+add_action( 'shoestrap_include_files', 'shoestrap_edd_include_files' );
