c1cb687f2972ee467e95e09fb1ac62af86887a90
Aristeides Stathopoulos <aristath@gmail.com>
2014-03-07 18:00:32 +0200

    bugfix the cart widget templates

--- edd_templates/widget-cart-checkout.php
+++ edd_templates/widget-cart-checkout.php
@@ -10,8 +10,12 @@
 ?>
 <?php global $ss_framework; ?>
 <?php echo $ss_framework->open_row( 'li', null, 'cart_item edd_subtotal' ); ?>
-	<h4><?php echo __( 'Subtotal:', 'edd' ). " <span class='subtotal'>" . edd_currency_filter( edd_format_amount( edd_get_cart_subtotal() ) ); ?></span></h4>
+	<?php echo $ss_framework->open_col( 'span', array( 'tablet' => 12 ) ); ?>
+		<h4><?php echo __( 'Subtotal:', 'edd' ). " <span class='subtotal'>" . edd_currency_filter( edd_format_amount( edd_get_cart_subtotal() ) ); ?></span></h4>
+	<?php echo $ss_framework->close_col( 'span' ); ?>
 <?php echo $ss_framework->close_row( 'li' ); ?>
 <?php echo $ss_framework->open_row( 'li', null, 'cart_item edd_checkout' ); ?>
-	<a class="<?php echo $ss_framework->button_classes( 'success', 'medium', null, 'btn-block expand' ); ?>" href="<?php echo edd_get_checkout_uri(); ?>"><?php _e( 'Checkout', 'edd' ); ?></a>
+	<?php echo $ss_framework->open_col( 'span', array( 'tablet' => 12 ) ); ?>
+		<a class="<?php echo $ss_framework->button_classes( 'success', 'medium', null, 'btn-block expand' ); ?>" href="<?php echo edd_get_checkout_uri(); ?>"><?php _e( 'Checkout', 'edd' ); ?></a>
+	<?php echo $ss_framework->close_col( 'span' ); ?>
 <?php echo $ss_framework->close_row( 'li' );
--- edd_templates/widget-cart-item.php
+++ edd_templates/widget-cart-item.php
@@ -16,5 +16,8 @@
 	<?php echo $ss_framework->open_col( 'span', array( 'tablet' => 3 ), null, 'edd-cart-item-price' ); ?>
 		{item_amount}&nbsp;
 	<?php echo $ss_framework->close_col( 'span' ); ?>
-	<a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="<?php echo $ss_framework->column_classes( array( 'tablet' => 2 ), 'string' ); ?> <?php echo $ss_framework->button_classes( 'danger', 'extra-small', null, 'right pull-right edd-remove-from-cart' ); ?>"><i class="el-icon-remove"></i></a>
-<?php echo $ss_framework->close_row( 'li' );
+	<?php echo $ss_framework->open_col( 'span', array( 'tablet' => 2 ), null, 'edd-cart-item-remove' ); ?>
+		<a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="<?php echo $ss_framework->button_classes( 'danger', 'extra-small', null, 'right pull-right edd-remove-from-cart' ); ?>"><i class="el-icon-remove"></i></a>
+	<?php echo $ss_framework->close_col( 'span' ); ?>
+<?php echo $ss_framework->close_row( 'li' ); ?>
+<hr>
