c5cc1493c29b474c72ab85877d8c397a63e3e096
Aristeides Stathopoulos <aristath@gmail.com>
2014-03-07 20:29:14 +0200

    implementing make_dropdown_button

--- templates/shoestrap-edd-sorting.php
+++ templates/shoestrap-edd-sorting.php
@@ -1,23 +1,16 @@
-<?php global $ss_framework; ?>
-<div class="pull-left sort">
-	<div class="btn-group">
-		<button type="button" class="btn btn-default btn-name dropdown-toggle" data-toggle="dropdown">
-			<span class="name"><?php _e( 'Name', 'shoestrap_edd' ); ?></span> <span class="caret"></span>
-		</button>
-		<ul class="dropdown-menu" role="menu">
-			<li class="default"><a href="#"><i class="el-icon-remove"></i> <?php _e( 'Default', 'shoestrap_edd' ); ?></a></li>
-			<li class="false"><a href="#name"><i class="el-icon-chevron-down"></i> <?php _e( 'Descending', 'shoestrap_edd' ); ?></a></li>
-			<li class="true"><a href="#name"><i class="el-icon-chevron-up"></i> <?php _e( 'Ascending', 'shoestrap_edd' ); ?></a></li>
-		</ul>
-	</div>
-	<div class="btn-group">
-		<button type="button" class="btn btn-default btn-price dropdown-toggle" data-toggle="dropdown">
-			<span class="name"><?php _e( 'Price', 'shoestrap_edd' ); ?></span>	<span class="caret"></span>
-		</button>
-		<ul class="dropdown-menu" role="menu">
-			<li class="default"><a href="#"><i class="el-icon-remove"></i> <?php _e( 'Default', 'shoestrap_edd' ); ?></a></li>
-			<li class="false"><a href="#price"><i class="el-icon-chevron-down"></i> <?php _e( 'Descending', 'shoestrap_edd' ); ?></a></li>
-			<li class="true"><a href="#price"><i class="el-icon-chevron-up"></i> <?php _e( 'Ascending', 'shoestrap_edd' ); ?></a></li>
-		</ul>
-	</div>
-</div>
+<?php
+
+global $ss_framework;
+
+echo '<div class="pull-left left sort">';
+
+
+	$content  = '<li class="default"><a href="#"><i class="el-icon-remove"></i> ' . __( 'Default', 'shoestrap_edd' ) . '</a></li>';
+	$content .= '<li class="false"><a href="#name"><i class="el-icon-chevron-down"></i> ' . __( 'Descending', 'shoestrap_edd' ) . '</a></li>';
+	$content .= '<li class="true"><a href="#name"><i class="el-icon-chevron-up"></i> ' . __( 'Ascending', 'shoestrap_edd' ) . '</a></li>';
+
+	echo $ss_framework->make_dropdown_button( 'default', 'medium', null, 'btn-name', __( 'Name', 'shoestrap_edd' ), $content );
+
+	echo $ss_framework->make_dropdown_button( 'default', 'medium', null, 'btn-price', __( 'Price', 'shoestrap_edd' ), $content );
+
+echo '</div>';
