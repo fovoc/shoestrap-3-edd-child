13d3c09a5a80f1d47734550528ff5dfb92df8099
fovoc <fovoc.otinanai@gmail.com>
2014-03-08 15:11:08 +0200

    working filters & sorters

--- templates/shoestrap-edd-sorting.php
+++ templates/shoestrap-edd-sorting.php
@@ -5,12 +5,16 @@ global $ss_framework;
 echo '<div class="pull-left left sort">';
 
 
-	$content  = '<li class="default"><a href="#"><i class="el-icon-remove"></i> ' . __( 'Default', 'shoestrap_edd' ) . '</a></li>';
-	$content .= '<li class="false"><a href="#name"><i class="el-icon-chevron-down"></i> ' . __( 'Descending', 'shoestrap_edd' ) . '</a></li>';
-	$content .= '<li class="true"><a href="#name"><i class="el-icon-chevron-up"></i> ' . __( 'Ascending', 'shoestrap_edd' ) . '</a></li>';
+	$content_name  = '<li class="default"><a href="#"><i class="el-icon-remove"></i> ' . __( 'Default', 'shoestrap_edd' ) . '</a></li>';
+	$content_name .= '<li class="false"><a href="#name"><i class="el-icon-chevron-down"></i> ' . __( 'Descending', 'shoestrap_edd' ) . '</a></li>';
+	$content_name .= '<li class="true"><a href="#name"><i class="el-icon-chevron-up"></i> ' . __( 'Ascending', 'shoestrap_edd' ) . '</a></li>';
 
-	echo $ss_framework->make_dropdown_button( 'default', 'medium', null, 'left btn-name', __( 'Name', 'shoestrap_edd' ), $content );
+	$content_price  = '<li class="default"><a href="#"><i class="el-icon-remove"></i> ' . __( 'Default', 'shoestrap_edd' ) . '</a></li>';
+	$content_price .= '<li class="false"><a href="#price"><i class="el-icon-chevron-down"></i> ' . __( 'Descending', 'shoestrap_edd' ) . '</a></li>';
+	$content_price .= '<li class="true"><a href="#price"><i class="el-icon-chevron-up"></i> ' . __( 'Ascending', 'shoestrap_edd' ) . '</a></li>';
 
-	echo $ss_framework->make_dropdown_button( 'default', 'medium', null, 'left btn-price', __( 'Price', 'shoestrap_edd' ), $content );
+	echo $ss_framework->make_dropdown_button( 'default', 'medium', 'left btn-name', null, '<span class="name">' . __( 'Name', 'shoestrap_edd' ) . '</span>', $content_name );
+
+	echo $ss_framework->make_dropdown_button( 'default', 'medium', 'left btn-price', null, '<span class="name">' . __( 'Price', 'shoestrap_edd' ) . '</span>', $content_price );
 
 echo '</div>';
